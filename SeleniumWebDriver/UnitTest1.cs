1ab679a2979c4f70caa80d82bade72b4c317f69e
funkycodeblog <funkycodeblog@gmail.com>
2018-11-14 23:30:45 +0100

    First cat's behind fence

--- SeleniumWebDriver/UnitTest1.cs
+++ SeleniumWebDriver/UnitTest1.cs
@@ -1,14 +1,25 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumWebDriver
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
+            // Create an instance of the driver
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http:/google.com");
+
+            var input = driver.FindElement(By.Id("lst-ib"));
+            input.SendKeys("calculator");
+            input.SendKeys(Keys.Enter);
+      
+
         }
     }
 }
